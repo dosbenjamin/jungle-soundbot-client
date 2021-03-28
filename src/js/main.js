4edbe7e77187e4b0bcd3d744de8f7575ec56d1f6
dosbenjamin <dosbenjamin@gmail.com>
2021-03-28 15:52:26 +0200

    feat(upload): add upload request on button click

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+const $form = document.querySelector('.js-form')
+const $button = document.querySelector('.js-submit')
+
+const upload = async () => {
+  await fetch('https://jungle-soundbot-api.herokuapp.com', {
+    method: 'POST',
+    body: new FormData($form),
+    mode: 'no-cors'
+  })
+}
+
+$button.addEventListener('click', upload)
